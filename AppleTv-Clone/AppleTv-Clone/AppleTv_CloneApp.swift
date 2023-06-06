cbde33f63c0b2067ef71396b5d4bfa8b5ab7027c
Gabriel Rossi <gabrieldesouzarossi123456@gmail.com>
2023-06-06 11:46:20 -0300

    add project

---
+++ AppleTv-Clone/AppleTv-Clone/AppleTv_CloneApp.swift
@@ -0,0 +1,18 @@
+//
+//  AppleTv_CloneApp.swift
+//  AppleTv-Clone
+//
+//  Created by Gabriel Rossi on 06/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleTv_CloneApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleTv-Clone/AppleTv-Clone/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  AppleTv-Clone
+//
+//  Created by Gabriel Rossi on 06/06/23.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack{
+            Text("wefw")
+        }
+        
+    }
+    
+}
+        
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+            ContentView()
+        
+    }
+}
